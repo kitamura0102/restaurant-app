321c558d0c511d829feb923e952540a45a2cb9d2
kitamura0102 <raul_almonte001@hotmail.com>
2025-02-18 23:47:43 -0300

    I was able to link the buttom to the items I clicked

--- src/data.js
+++ src/data.js
@@ -1,23 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 6,
-        emoji: "🍺",
-        id: 2
-    }
-]
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    price: 14,
+    emoji: "🍕",
+    uuid: uuidv4()
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    uuid: uuidv4(),
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 6,
+    emoji: "🍺",
+    uuid: uuidv4(),
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,48 +1,67 @@
 import "./style.css";
 import { menuArray } from "./data";
 
-// function addItem() {
-//   const addBtn = document.getElementById("addBtn");
-//   addBtn.addEventListener("click", function (e) {
-//     console.log(e.target);
-//   });
-// }
-function renderTop() {
-  return `<div class="upper-bg">
-          <h1>Raul's Diner</h1>
-          <p>The best burgers and pizzas in town</p>
-        </div>`;
+const cart = []
+
+document.addEventListener('click', function(e){
+  if(e.target.dataset.add){
+    addItems(e.target.dataset.add)
+  }
+})
+
+
+function addItems(itemsId){
+  const targetItemObj = menuArray.filter(function(item){
+    return item.uuid === itemsId
+  })[0]
+
+  if(targetItemObj){
+    cart.push(targetItemObj.name , targetItemObj.price)
+  }
+  
+  
 }
 function renderMenu() {
-  return menuArray.map(function (item) {
-    return `<div class="item-box">
+  let feedHtml = "";
+
+  menuArray.map(function (item) {
+    feedHtml += `
+    <div class="item-box">
             <span>${item.emoji}</span>
             <div class="items">
               <h2 class="item">${item.name}</h2>
               <p class="ingredients">${item.ingredients.join(", ")}</p>
               <h3 class="price">$${item.price}</h3>
             </div>
-            <button id="addBtn"><i class="fa-solid fa-plus fa-M"></i></button>
-          </div>`;
+            <button class="add-btn" id="addBtn" data-add='${item.uuid}'><i class="fa-solid fa-plus fa-M "></i></button>
+    </div>
+          `;
   });
+  return feedHtml;
 }
 function renderBottom() {
-  return `<div class="order">
-          <h2 class="title-order">Your Order</h2>
+
+  const orderDetails = cart.forEach(function(item){
+    let totalPrice =+ item.price
+    `<div class="order hidden">
+          <h2 class="title-order">Your Order Details</h2>
           <div class="order-items">
-            <h2 class="item-order">Pizza</h2>
-            <span class="price-item">$14</span>
+            <h2 class="item-order">${item.name}</h2>
+            <span class="price-item">$${item.price}</span>
           </div>
           <div class="total-price">
             <h2>Total Price:</h2>
-            <span class="price-item-order">$14</span>
+            <span class="price-item-order">$${totalPrice}</span>
           </div>
           <button>Complete Order</button>
         </div>`;
+  })
+
+  console.log(orderDetails)
 }
 
 function render() {
-  return renderTop() + renderMenu().join("") + renderBottom();
+  return renderMenu() + renderBottom();
 }
 
 document.getElementById("container").innerHTML = render();
