06f108cfd7e4fc609d0314d6951d49d28e0d02e5
kitamura0102 <raul_almonte001@hotmail.com>
2025-02-23 23:20:47 -0300

    Added the login when adding an item. only missing to bulk it if there are more than 1 item

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 import { menuArray } from "./data";
 
 const cart = []
+let order = []
 
 document.addEventListener('click', function(e){
   if(e.target.dataset.add){
@@ -10,17 +11,10 @@ document.addEventListener('click', function(e){
 })
 
 
-function addItems(itemsId){
-  const targetItemObj = menuArray.filter(function(item){
-    return item.uuid === itemsId
-  })[0]
 
-  if(targetItemObj){
-    cart.push(targetItemObj.name , targetItemObj.price)
-  }
-  
-  
-}
+
+
+//Render the menu
 function renderMenu() {
   let feedHtml = "";
 
@@ -39,29 +33,73 @@ function renderMenu() {
   });
   return feedHtml;
 }
-function renderBottom() {
+//Every time an item is clicked, do something / identify the item clicked
+function addItems(itemsId){
+  
+  const targetItemObj = menuArray.filter(function(item){
+    return item.uuid === itemsId
+  })[0]
 
-  const orderDetails = cart.forEach(function(item){
-    let totalPrice =+ item.price
-    `<div class="order hidden">
-          <h2 class="title-order">Your Order Details</h2>
-          <div class="order-items">
-            <h2 class="item-order">${item.name}</h2>
-            <span class="price-item">$${item.price}</span>
-          </div>
-          <div class="total-price">
-            <h2>Total Price:</h2>
-            <span class="price-item-order">$${totalPrice}</span>
-          </div>
-          <button>Complete Order</button>
-        </div>`;
-  })
-
-  console.log(orderDetails)
+  if(targetItemObj){
+    cart.push(targetItemObj.price)
+    order.push(renderItemsBottom(targetItemObj)) 
+    render() 
+    renderBodyBottom()
+  }
+  
+  
 }
 
+
+//sum the cart in order to get the total of it
+function sumCart(){
+  let price = 0;
+  let totalPrice = cart.reduce((accumulator, currentValue)=> accumulator + currentValue, price,)
+  return totalPrice
+}
+
+//render the items in the cart in order to process the payment
+function renderItemsBottom(items){
+  let buttomRendered = '';
+  if(cart.length > 0){
+    buttomRendered =  `
+    <div class="order-items">
+      <h2 class="item-order">${items.name}</h2>
+      <span class="price-item">$${items.price}</span>
+    </div>
+    `;
+  }
+    
+return buttomRendered
+
+}
+
+//render the main body
+function renderBodyBottom() {
+  let buttomRendered = `<div class="order">
+    <h2 class="title-order">Your Order Details</h2>
+    <div class="list-of-items">
+    ${order.join('')}
+    </div>
+    <div class="total-price">
+      <h2>Total Price:</h2>
+      <span class="price-item-order">$${sumCart()}</span>
+    </div>
+    <button>Complete Order</button>
+  </div>`
+  
+    
+  
+return buttomRendered
+
+  
+}
+//Render
 function render() {
-  return renderMenu() + renderBottom();
+
+  document.getElementById("container").innerHTML =  renderMenu() + renderBodyBottom()
+ 
 }
 
-document.getElementById("container").innerHTML = render();
+
+render()
